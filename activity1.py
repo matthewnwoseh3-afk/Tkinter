aa5c629c5ac32af4dc61177d2ec5d42dd931a293
matthewnwoseh3-afk <matthewnwoseh3@gmail.com>
2026-05-18 18:35:08 -0500

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,5 +1,19 @@
 from tkinter import *
-window = Tk()
-window.title("Demo window")
-window.geometry('400x300')
-window.mainloop() 
+root = Tk()
+root.title("Number Pad")
+root.geometry('250x300')
+nums = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+for i  in range(4):
+    root.columnconfigure(i, weight=1, minsize=50)
+    root.rowconfigure(i, weight=1, minsize=50)
+    for j in range(0, 3):
+        frame = Frame(
+            master=root,
+            relief=SUNKEN,
+            borderwidth=1
+
+        )
+        frame.grid(row=i, column=j)
+        label= Label(master=frame, text=nums[i][j], bg='lightblue')
+        label.pack(padx=10, pady=3)
+root.mainloop()
